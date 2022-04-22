ae8832cb39255e4faab3daaac133271a9171dc6c
Tzvi-H <16guitar@gmail.com>
2022-04-22 12:51:30 -0500

    completed 7.5

--- src/App.js
+++ src/App.js
@@ -84,6 +84,14 @@ const CreateNew = (props) => {
     navigate('/anecdotes')
   }
 
+  const handleReset = e => {
+    e.preventDefault()
+    
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -101,6 +109,7 @@ const CreateNew = (props) => {
           <input {...info} name="info"/>
         </div>
         <button>create</button>
+        <button onClick={handleReset}>reset</button>
       </form>
     </div>
   )
